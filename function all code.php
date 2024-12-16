516a30809ff56af1fd0c8e0c2c1344e0421dfc58
vishal-vt <vishalmishra70524@gmail.com>
2024-12-16 10:28:33 +0530

    function

---
+++ function all code.php
@@ -0,0 +1,15 @@
+<?php
+function calculate ($a,$b,$op){
+    switch($op){
+        case "+":
+            return $a + $b;
+        case "-":
+                return $a + $b;
+        case "*":
+                return $a + $b;
+        case "/":
+                return $a + $b;    
+    }
+}
+echo calculate(10,20,'-');
+?>
--- prime number in.php
+++ prime number in.php
@@ -1,18 +1,18 @@
 <?php
-function prime($num){
+// function prime($num){
 
-    if( $num % 3 == 0 && $num % 7==0){
-        echo "this is not prime number";
-    }
-    else if($num % 2 == 0 && $num % 5 == 0){
-        echo "this is not prime number";
-     } 
-    else if($num % 1==0 && $num % $num==0){
-    echo "this is prime number";
-    }
+//     if( $num % 3 == 0 && $num % 7==0){
+//         echo "this is not prime number";
+//     }
+//     else if($num % 2 == 0 && $num % 5 == 0){
+//         echo "this is not prime number";
+//      } 
+//     else if($num % 1==0 && $num % $num==0){
+//     echo "this is prime number";
+//     }
     
-    else {
-        echo "this is wrong";
-    }
-}
-prime(9);
+//     else {
+//         echo "this is wrong";
+//     }
+// }
+// prime(9);
